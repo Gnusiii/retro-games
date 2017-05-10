54f46d5a0e9c88c0e9b446e2cbb8011fb588748c
Hjordis Gudjonsdottir <hjordis.ester@gmail.com>
2017-05-10 11:03:31 +0000

    bætti við skrolli frá nav og bætti við að hægt að væri að klikka á logo og info

--- js/scroll.js
+++ js/scroll.js
@@ -1,39 +1,19 @@
-	/*$(function() {
 
 
-	$.scrollify({
+$(document).ready(() =>{
+	$(()=> {
+  		$.scrollify({
 		section:".panel",
-		//sectionName:false,
-		scrollSpeed:1100,
-		after:function(i) {
-*/
-
-/*
-			if(i===2) {
-				$.scrollify.setOptions({
-					easing:"easeOutExpo"
-				});
-			}*/
-/*
-		}
+		sectionName: "section-name",    
+    	scrollSpeed:1500,
+    	interstitialSection:".header,.footer"
+  		});
 	});
 
-	$(".scroll,.scroll-btn").click(function(e) {
-		e.preventDefault();
-
-		$.scrollify.next();
-
-
-
-	});
-
-});*/
-$(()=> {
-  $.scrollify({
-		section:".panel",
-    sectionName:false,
-    scrollSpeed:1500,
-    interstitialSection:".header,.footer"
-  });
+	$('.link').click(function() {
+		const index = parseInt(this.getAttribute("data-index"))
+		$.scrollify.move(index);		
+	})
 });
+
 
